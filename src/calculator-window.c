a5521aa04141e72d22add4cddb5048e5c86d665e
TXG0Fk3 <137466333+TXG0Fk3@users.noreply.github.com>
2023-11-14 12:54:46 -0300

    meson.build && calculator.gresource.xml && calculator-window.c -- fixed

--- src/calculator-window.c
+++ src/calculator-window.c
@@ -10,7 +10,7 @@ void app_activate(GtkApplication *app){
   GtkBuilder *builder;
   char vle1[10], opr, vle2[10];
   
-  builder = gtk_builder_new_from_resource("/com/github/TXG0Fk3/Simple-Calculator/calculator-window.ui");
+  builder = gtk_builder_new_from_resource("/com/github/TXG0Fk3/Simple-Calculator/src/calculator-window.ui");
 
   win = GTK_WIDGET(gtk_builder_get_object(builder, "win"));
   gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
