754ac8faa7edf5e48b095531915f230cddc3428a
TXG0Fk3 <137466333+TXG0Fk3@users.noreply.github.com>
2023-11-18 15:09:09 -0300

    float and ()

--- src/calculate.c
+++ src/calculate.c
@@ -1,48 +1,66 @@
 #include "calculate.h"
 
 double calculate(char *expression){
-    long double stack[512];
-    int top = 0;
-    char *postfix = malloc(sizeof(char) * strlen(expression)), *p = postfix;
+  double stack[512];
+  int top = 0;
+  char *postfix = malloc(sizeof(char) * (strlen(expression) * 1.5)), *p = postfix;
 
-    for (char *s = expression; *s; s++){
-        if (isdigit(*s)) {
-            while (isdigit(*s)) *p++ = *s++;
-            *p++ = ' ';
-        }
-
-        if (*s == '+' || *s == '-' || *s == '*' || *s == '/') {
-            while (top && (stack[top-1] == '*' || stack[top-1] == '/')){
-                *p++ = stack[--top];
-                *p++ = ' ';
-            }
-            stack[top++] = *s;
-        }
-    }
-
-    while(top){
-      *p++ = stack[--top];
+  for (char *s = expression; *s; s++){
+    if (isdigit(*s) || *s == '.'){
+      while (isdigit(*s) || *s == '.') *p++ = *s++;
       *p++ = ' ';
     }
-
-    *p = 0;
-
-    for (char *s = postfix; *s; s++) {
-        if (isdigit(*s)) {
-            double value = 0;
-            while (isdigit(*s)) value = 10 * value + *s++ - '0';
-            stack[top++] = value;
-        }
-
-        if (*s == '+' || *s == '-' || *s == '*' || *s == '/') {
-            double b = stack[--top], a = stack[--top];
-            if (*s == '+') stack[top++] = a + b;
-            if (*s == '-') stack[top++] = a - b;
-            if (*s == '*') stack[top++] = a * b;
-            if (*s == '/') stack[top++] = a / b;
-        }
+    if (*s == '+' || *s == '-' || *s == '*' || *s == '/'){
+      if (top && (stack[top-1] == '*' || stack[top-1] == '/')){
+        *p++ = stack[--top];
+        *p++ = ' ';
+      }
+      stack[top++] = *s;
     }
 
-    free(postfix);
-    return stack[0];
+    if (*s == '('){
+      stack[top++] = *s;
+    }
+
+    if (*s == ')'){
+      while (top && stack[top-1] != '('){
+        *p++ = stack[--top];
+        *p++ = ' ';
+      }
+      top--;
+    }
+  }
+
+  while (top){
+    *p++ = stack[--top];
+    *p++ = ' ';
+  }
+
+  for (char *s = postfix; *s; s++){
+    if (isdigit(*s) || *s == '.'){
+      double value = 0, factor = 1;
+      while (isdigit(*s)){
+        value = 10 * value + *s++ - '0';
+      }
+      if (*s == '.'){
+        s++;
+        while(isdigit(*s)){
+          factor /= 10;
+          value += (*s++ - '0') * factor;
+        }
+      }
+      stack[top++] = value;
+    }
+
+    if (*s == '+' || *s == '-' || *s == '*' || *s == '/'){
+      double b = stack[--top], a = stack[--top];
+      if (*s == '+') stack[top++] = a + b;
+      if (*s == '-') stack[top++] = a - b;
+      if (*s == '*') stack[top++] = a * b;
+      if (*s == '/') stack[top++] = a / b;
+    }
+  }
+
+  free(postfix);
+  return stack[0];
 }
